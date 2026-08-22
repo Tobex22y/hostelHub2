21865b0bd691c321b01924eee45dfac938350cf0
Tobex22y <150237403+Tobex22y@users.noreply.github.com>
2026-08-22 21:04:03 +0100

    Add session handling to logout functionality

--- backend/api/admin/logout.php
+++ backend/api/admin/logout.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once __DIR__ . "/../config/db.php";
+require_once __DIR__ . "/../config/session_handler.php";
+
+$handler = new DBSessionHandler();
+session_set_save_handler($handler, true);
+
 session_start();
 session_destroy();
 
