458297fb956976235c7afee4b62cd5942e58c3b3
Tobex22y <150237403+Tobex22y@users.noreply.github.com>
2026-08-22 20:57:25 +0100

    Add session management to reserve.php

--- backend/api/student/reserve.php
+++ backend/api/student/reserve.php
@@ -1,4 +1,10 @@
 <?php
+require_once __DIR__ . "/../config/db.php";
+require_once __DIR__ . "/../config/session_handler.php";
+
+$handler = new DBSessionHandler();
+session_set_save_handler($handler, true);
+
 session_start();
 
 header("Access-Control-Allow-Origin: http://localhost");
