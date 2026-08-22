bf8e6202240e55e5a932c44174231c42c9365edf
Tobex22y <150237403+Tobex22y@users.noreply.github.com>
2026-08-22 20:36:14 +0100

    Update allocation.php

--- backend/api/student/allocation.php
+++ backend/api/student/allocation.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/../config/db.php";
 require_once __DIR__ . "/../config/session_handler.php";
+require_once __DIR__ . "/../config/session_bootstrap.php";
 
 $handler = new DBSessionHandler();
 session_set_save_handler($handler, true);
