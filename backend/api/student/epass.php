f7ed24758993f824897d57688c7aa1423d182f61
Tobex22y <150237403+Tobex22y@users.noreply.github.com>
2026-08-22 20:58:44 +0100

    Add database and session handler requirements

--- backend/api/student/epass.php
+++ backend/api/student/epass.php
@@ -1,4 +1,10 @@
 <?php
+require_once __DIR__ . "/../config/db.php";
+require_once __DIR__ . "/../config/session_handler.php";
+
+$handler = new DBSessionHandler();
+session_set_save_handler($handler, true);
+
 session_start();
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Pragma: no-cache');
